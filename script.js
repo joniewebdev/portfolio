adeb5ae9b96183c83e4278bf22937648c78feae4
joniewebdev <joniebechayda27@gmail.com>
2024-07-15 12:35:20 +0800

    major update

---
+++ script.js
@@ -0,0 +1,51 @@
+function scrollToSection(sectionId) {
+    console.log(`Attempting to scroll to section: ${sectionId}`);
+    const section = document.getElementById(sectionId);
+    if (section) {
+        console.log(`Section found: ${sectionId}`);
+        section.scrollIntoView({ behavior: 'smooth' });
+    }
+    else {
+        console.log(`Section not found: ${sectionId}`);
+    }
+}
+
+//Scroll to Top button
+let mybutton = document.getElementById("toTop");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+
+//image slider
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("yamarobot-image");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex-1].style.display = "block";
+}
